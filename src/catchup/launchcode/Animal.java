ae18cd267bbcac2b1ebbda24cf077aaacd2484a8
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-19 17:44:34 -0400

    Initial commit

---
+++ src/catchup/launchcode/Animal.java
@@ -0,0 +1,17 @@
+package catchup.launchcode;
+
+import java.util.*;
+
+public class Animal {
+    String name;
+    Integer age;
+    Date date;
+    Double weight;
+
+    public Animal(String aName, Integer age, Date date, Double weight) {
+        this.name = aName;
+        this.age = aAge;
+        this.date = aDate;
+        this.weight = aWeight;
+    }
+}
---
+++ src/catchup/launchcode/Main.java
@@ -0,0 +1,13 @@
+package catchup.launchcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Animal felix = new Animal();
+    felix.name = "Felix";
+    felix.age = 10;
+    felix.date = new Date();
+
+
+    }
+}
