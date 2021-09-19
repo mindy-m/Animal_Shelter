c3b080fd7fec190f0440d4f65d204133046dc3c2
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-19 18:12:03 -0400

    State at the end of the Catchup Corral session

--- src/catchup/launchcode/Main.java
+++ src/catchup/launchcode/Main.java
@@ -3,10 +3,10 @@ package catchup.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-	Animal felix = new Animal();
-    felix.name = "Felix";
-    felix.age = 10;
-    felix.date = new Date();
+//	  Animal felix = new Animal();
+//    felix.name = "Felix";
+//    felix.age = 10;
+//    felix.date = new Date();
 
 
     }
